09c79f6719977a072927f297842e63eec3efe740
Lucio Henrique <lucions96@gmail.com>
2024-07-25 14:20:42 -0300

    init project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const STyled = styled.Text`
+  background: red;
+`;
+
+export const Bootstrap = () => {
+  return <STyled>Hello world</STyled>;
+};
